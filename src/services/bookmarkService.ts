8ce4a451e2f831c1d6fd7d46313d1f9704d18f6e
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 00:50:32 +0700

    edit url api

--- src/services/bookmarkService.ts
+++ src/services/bookmarkService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BOOKMARK_API = 'http://localhost:5000/api';
+const BOOKMARK_API = 'https://f05cf22fe458.ngrok.io/api';
 export const getBookmarks = async () => {
   try {
     const bookmarks = await axios.get(`${BOOKMARK_API}/bookmark`);
