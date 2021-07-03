0b79fd5961cd281d95dd2fe280ff179f6afcd813
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 13:43:23 +0700

    call external func

--- src/controllers/lineController.ts
+++ src/controllers/lineController.ts
@@ -3,6 +3,7 @@ import express, { Application, Request, Response } from 'express';
 import * as BookmarkService from '../../src/services/bookmarkService';
 import * as MovieService from '../../src/services//TMDB/movieService';
 import { cardMedia } from '../../src/utils/messageHelper';
+import { getMoviePopular } from '../utils/handleTextHelper';
 
 export const textEventHandler = async (event: WebhookEvent , client:Client): Promise<MessageAPIResponseBase | undefined> => {
   // Process all variables here.
@@ -17,15 +18,7 @@ export const textEventHandler = async (event: WebhookEvent , client:Client): Pro
   
 
   if(text === 'movie popular') {
-    const movies = await MovieService.discoverMovie();
-    console.log(movies);
-    let text = [];
-    for(let i = 0; i<5 ; i++) {
-      const response: FlexMessage = cardMedia(movies.results[i]);
-      text.push(response);
-      
-    }
-    await client.replyMessage(replyToken, text);
+    await getMoviePopular(replyToken , client);
   }
 
 
