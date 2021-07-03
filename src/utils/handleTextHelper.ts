9afbd480158b3f560aed9cfdb245217004c6f7e0
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 13:27:52 +0700

    get movie popular

---
+++ src/utils/handleTextHelper.ts
@@ -0,0 +1,24 @@
+import { Client, FlexMessage, TextMessage, WebhookEvent } from '@line/bot-sdk';
+import express, { Application, Request, Response } from 'express';
+import * as BookmarkService from '../../src/services/bookmarkService';
+import * as MovieService from '../../src/services//TMDB/movieService';
+import { cardMedia } from '../../src/utils/messageHelper';
+
+export const getMoviePopular = async (replyToken:string , client:Client) => {
+  const movies = await MovieService.discoverMovie();
+    console.log(movies);
+    let replyArr = [];
+    for(let i = 0; i<movies.results.length ; i++) {
+      if(i < 6) {
+        const response: FlexMessage = cardMedia(movies.results[i]);
+        replyArr.push(response);
+      } else {
+        const text: TextMessage = {
+          type : "text",
+          text : movies.results[i].title
+        }
+        replyArr.push(text);
+      }
+    }
+    await client.replyMessage(replyToken, replyArr);
+}
