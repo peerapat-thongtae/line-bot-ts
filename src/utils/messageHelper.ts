68ffc6a1e1923252475e7eec6b0e7bccaabac711
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-04 04:42:26 +0700

    loop in carousel

--- src/utils/messageHelper.ts
+++ src/utils/messageHelper.ts
@@ -1,4 +1,4 @@
-import { FlexCarousel, FlexMessage } from "@line/bot-sdk";
+import { FlexBubble, FlexCarousel, FlexMessage } from "@line/bot-sdk";
 import { MovieInterface } from "../models/movieModel";
 
 
@@ -132,256 +132,140 @@ export const cardMedia = (movie:MovieInterface) => {
 }
 
 export const cardCarousel = async (datas:any) => {
-  const movie = datas[0];
+  let i = 0;
+  const carouselArr:Array<FlexBubble> = [];
+
+  for(let i = 0 ; i<10 ; i++ ) {
+    carouselArr.push(
+      {
+      type: "bubble",
+      size: "giga",
+      direction : "ltr",
+      hero : {
+        "type": "image",
+        "url": `${process.env.TMDB_IMAGE_PATH}/${datas[i].backdrop_path}`,
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": `${datas[i].title}`,
+            "weight": "bold",
+            "size": "xl"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "lg",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Title",
+                    "color": "#aaaaaa",
+                    "size": "sm",
+                    "flex": 1
+                  },
+                  {
+                    "type": "text",
+                    "text": datas[i].title,
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Release date",
+                    "color": "#aaaaaa",
+                    "size": "sm",
+                    "flex": 1
+                  },
+                  {
+                    "type": "text",
+                    "text": datas[i].release_date,
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Overview",
+                    "color": "#aaaaaa",
+                    "size": "sm",
+                    "flex": 1
+                  },
+                  {
+                    "type": "text",
+                    "text": datas[i].overview,
+                    "wrap": true,
+                    "color": "#666666",
+                    "size": "sm",
+                    "flex": 5
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "button",
+            "style": "link",
+            "height": "sm",
+            "action": {
+              "type": "uri",
+              "label": "Detail",
+              "uri": `${process.env.TMDB_URL}/movie/${datas[i].id}`
+            }
+          },
+          
+          {
+            "type": "spacer",
+            "size": "sm"
+          }
+        ],
+        "flex": 0
+      }
+      }
+    );
+  }
   const carousel:FlexMessage = {
     "type": "flex",
     "altText" : "test",
     "contents" : {
       type : "carousel",
-      "contents" : [
-        {
-          type: "bubble",
-          size: "giga",
-          direction : "ltr",
-          hero : {
-            "type": "image",
-            "url": `${process.env.TMDB_IMAGE_PATH}/${movie.backdrop_path}`,
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": `${movie.title} (${movie.vote_average})`,
-                "weight": "bold",
-                "size": "xl"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "margin": "lg",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Title",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.title,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Release date",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.release_date,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Overview",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.overview,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "button",
-                "style": "link",
-                "height": "sm",
-                "action": {
-                  "type": "uri",
-                  "label": "Detail",
-                  "uri": `${process.env.TMDB_URL}/movie/${movie.id}`
-                }
-              },
-              
-              {
-                "type": "spacer",
-                "size": "sm"
-              }
-            ],
-            "flex": 0
-          }
-        },
-        {
-          type: "bubble",
-          size: "giga",
-          direction : "ltr",
-          hero : {
-            "type": "image",
-            "url": `${process.env.TMDB_IMAGE_PATH}/${movie.backdrop_path}`,
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": `${movie.title} (${movie.vote_average})`,
-                "weight": "bold",
-                "size": "xl"
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "margin": "lg",
-                "spacing": "sm",
-                "contents": [
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Title",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.title,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Release date",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.release_date,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Overview",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": movie.overview,
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  }
-                ]
-              }
-            ]
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "button",
-                "style": "link",
-                "height": "sm",
-                "action": {
-                  "type": "uri",
-                  "label": "Detail",
-                  "uri": `${process.env.TMDB_URL}/movie/${movie.id}`
-                }
-              },
-              
-              {
-                "type": "spacer",
-                "size": "sm"
-              }
-            ],
-            "flex": 0
-          }
-        }
-      ]
+      "contents" : carouselArr
     }
   }
   return carousel;
