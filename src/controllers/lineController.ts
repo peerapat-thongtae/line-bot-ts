55e67bbb00f8586635c698f05b81c98d0ac8521b
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 13:46:02 +0700

    if length>5 reply only text

--- src/controllers/lineController.ts
+++ src/controllers/lineController.ts
@@ -3,7 +3,6 @@ import express, { Application, Request, Response } from 'express';
 import * as BookmarkService from '../../src/services/bookmarkService';
 import * as MovieService from '../../src/services//TMDB/movieService';
 import { cardMedia } from '../../src/utils/messageHelper';
-import { getMoviePopular } from '../utils/handleTextHelper';
 
 export const textEventHandler = async (event: WebhookEvent , client:Client): Promise<MessageAPIResponseBase | undefined> => {
   // Process all variables here.
@@ -18,7 +17,23 @@ export const textEventHandler = async (event: WebhookEvent , client:Client): Pro
   
 
   if(text === 'movie popular') {
-    await getMoviePopular(replyToken , client);
+    const movies = await MovieService.discoverMovie();
+    console.log(movies);
+    let replyArr = [];
+    for(let i = 0; i<5 ; i++) {
+      if(i < 6) {
+        const response: FlexMessage = cardMedia(movies.results[i]);
+        replyArr.push(response);
+      } else {
+        const text: TextMessage = {
+          type : "text",
+          text : movies.results[i].title
+        }
+        replyArr.push(text);
+      }
+      
+    }
+    await client.replyMessage(replyToken, replyArr);
   }
 
 
