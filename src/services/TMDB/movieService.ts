0bae999f4050f9b1c231e80a7f8c15fa81be5178
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 10:35:23 +0700

    edit url

--- src/services/TMDB/movieService.ts
+++ src/services/TMDB/movieService.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const TMDB_API_URL = process.env.TMDB_API_URL;
 const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 export const discoverMovie = async () => {
-    const movies = await axios.get(`${TMDB_API_URL}/3/movie/discover` , {
+    const movies = await axios.get(`${TMDB_API_URL}/3/discover/movie` , {
       headers: {
         Authorization: 'Bearer ' + TMDB_ACCESS_TOKEN
       }
