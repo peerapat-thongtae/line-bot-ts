66fb53ded1ea7c3534447957d9fc6f5c6d1964f6
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-03 10:32:30 +0700

    set access token

--- src/services/TMDB/movieService.ts
+++ src/services/TMDB/movieService.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const TMDB_API_URL = process.env.TMDB_API_URL;
+const TMDB_ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
 export const discoverMovie = async () => {
-  try {
-    const movies = await axios.get(`${TMDB_API_URL}/3/movie/discover`);
+    const movies = await axios.get(`${TMDB_API_URL}/3/movie/discover` , {
+      headers: {
+        Authorization: 'Bearer ' + TMDB_ACCESS_TOKEN
+      }
+    });
     return movies.data;
-
-  } catch(e) {
-    return e;
-  }
 }
