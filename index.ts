bde0e289de4920a782f1553f1b0ec54d739ab669
peerapat-thongtae <peera.thongtae@gmail.com>
2021-07-04 14:48:49 +0700

    test cron specific time

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ const client = new Client(clientConfig);
 
 const app: Application = express();
 
-cron.schedule('* * * * *', () => {
+cron.schedule('50 14 * * *', () => {
   console.log('Run task every minute');
 });
 
