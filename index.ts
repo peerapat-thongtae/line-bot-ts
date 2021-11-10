4739de710355430f9c05d66aba98fcea8255e36b
peerapat-thongtae <peera.thongtae@gmail.com>
2021-11-10 15:52:43 +0700

    font bold

--- index.ts
+++ index.ts
@@ -52,7 +52,7 @@ cron.schedule('00 16 * * *', async () => {
   
 });
 
-cron.schedule('52 15 * * *', async () => {
+cron.schedule('54 15 * * *', async () => {
   try {
     const result = await cinemaToday();
     let text = 'Movie in cinema today : ';
